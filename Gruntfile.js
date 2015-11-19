c5dd05590c0f039895ce9f50c7363648cca68aaa
Aaron Sawyer <me@aarondevon.com>
2015-11-19 11:30:53 -0600

    Update Gruntfile and package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,15 @@
 /**
  * Created by aaronsawyer on 11/19/15.
  */
+grunt.loadNpmTasks('grunt-responsive-original-images');
+
+grunt.initConfig({
+    responsive_images: {
+        options: {
+            // Task-specific options go here.
+        },
+        your_target: {
+            // Target-specific file lists and/or options go here.
+        },
+    },
+})
